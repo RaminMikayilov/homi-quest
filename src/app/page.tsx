dc8ae26b6e8fd91dde66a26255ea620c0ffd9ab8
RaminMikayilov <raminmikayilov2003@gmail.com>
2024-11-22 00:02:19 +0400

    feat: add UI components from shadcn UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 export default function HomePage() {
-  return <h1 className="text-3xl">HomePage</h1>;
+  return (
+    <>
+      <h1 className="text-3xl">HomePage</h1>
+      <Button>Test</Button>
+    </>
+  );
 }
