d5567fc662b398ae6252962858891904e354ae4f
RaminMikayilov <raminmikayilov2003@gmail.com>
2024-11-23 00:44:31 +0400

    feat: implement dropdown menu in Navbar with user links and user icon

--- src/components/custom/Navbar/LinksDropdown.tsx
+++ src/components/custom/Navbar/LinksDropdown.tsx
@@ -1,3 +1,34 @@
-export default function LinksDropdown() {
-  return <div>LinksDropdown</div>;
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { LuAlignLeft } from "react-icons/lu";
+import UserIcon from "./UserIcon";
+import { links } from "@/lib/constants/navLinks";
+import Link from "next/link";
+
+export default function DropdownMenuDemo() {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="outline" className="flex gap-4 max-w-[100px]">
+          <LuAlignLeft className="w-6 h-6" />
+          <UserIcon />
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="w-52" align="start" sideOffset={10}>
+        {links.map((link) => (
+          <DropdownMenuItem key={link.label}>
+            <Link href={link.href} className="capitalize w-full">
+              {link.label}
+            </Link>
+          </DropdownMenuItem>
+        ))}
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
 }
