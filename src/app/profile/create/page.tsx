c4b1b7e3a6d0630e9359e5e2c85ef9b6e78a8d2b
RaminMikayilov <raminmikayilov2003@gmail.com>
2024-12-02 00:17:57 +0400

    feat: update CreateProfilePage to include last name and username fields in the form

--- src/app/profile/create/page.tsx
+++ src/app/profile/create/page.tsx
@@ -2,11 +2,29 @@ import { SubmitButton } from "@/components/custom/form/buttons/SubmitButton";
 import FormContainer from "@/components/custom/form/FormContainer";
 import FormInput from "@/components/custom/form/FormInput";
 
+const createProfileAction = async (prevState: any, formData: FormData) => {
+  "use server";
+  const firstName = formData.get("firstName") as string;
+
+  return {
+    message: `Profile for ${firstName} created successfully`,
+  };
+};
+
 export default function CreateProfilePage() {
   return (
-    <FormContainer>
-      <FormInput name="firstName" type="text" />
-      <SubmitButton />
-    </FormContainer>
+    <section>
+      <h1 className="text-2xl font-semibold mb-8 capitalize"> new user</h1>
+      <div className="border p-8 rounded-md ">
+        <FormContainer action={createProfileAction}>
+          <div className="grid md:grid-cols-2 gap-4 mt-4">
+            <FormInput type="text" name="firstName" label="First Name" />
+            <FormInput type="text" name="lastName" label="Last Name" />
+            <FormInput type="text" name="username" label="Username" />
+          </div>
+          <SubmitButton text="Create Profile" className="mt-8" />
+        </FormContainer>
+      </div>
+    </section>
   );
 }
