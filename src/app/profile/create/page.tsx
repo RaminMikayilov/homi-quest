69530c76b16d6b2942cd55fc21154064289630ed
RaminMikayilov <raminmikayilov2003@gmail.com>
2024-12-02 00:04:39 +0400

    feat: add FormContainer component to manage form state and integrate it into CreateProfilePage

--- src/app/profile/create/page.tsx
+++ src/app/profile/create/page.tsx
@@ -1,11 +1,12 @@
 import { SubmitButton } from "@/components/custom/form/buttons/SubmitButton";
+import FormContainer from "@/components/custom/form/FormContainer";
 import FormInput from "@/components/custom/form/FormInput";
 
 export default function CreateProfilePage() {
   return (
-    <>
+    <FormContainer>
       <FormInput name="firstName" type="text" />
       <SubmitButton />
-    </>
+    </FormContainer>
   );
 }
