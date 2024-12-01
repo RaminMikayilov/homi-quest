46bc7e0973636dd386b77f9f7eee30268244add4
RaminMikayilov <raminmikayilov2003@gmail.com>
2024-12-01 13:45:02 +0400

    feat: add SubmitButton component

--- src/app/profile/create/page.tsx
+++ src/app/profile/create/page.tsx
@@ -1,5 +1,11 @@
+import { SubmitButton } from "@/components/custom/form/buttons/SubmitButton";
 import FormInput from "@/components/custom/form/FormInput";
 
 export default function CreateProfilePage() {
-  return <FormInput name="firstName" type="text" />;
+  return (
+    <>
+      <FormInput name="firstName" type="text" />
+      <SubmitButton />
+    </>
+  );
 }
