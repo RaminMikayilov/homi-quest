3ca9e8f21d310d86c2c600367370045dc22f4276
RaminMikayilov <raminmikayilov2003@gmail.com>
2024-12-21 22:22:33 +0400

    feat: add profile image fetching and display in UserIcon component

--- src/components/custom/Navbar/UserIcon.tsx
+++ src/components/custom/Navbar/UserIcon.tsx
@@ -1,5 +1,19 @@
 import { LuUser2 } from "react-icons/lu";
+import { fetchProfileImage } from "@/utils/actions";
+import Image from "next/image";
 
-export default function UserIcon() {
+export default async function UserIcon() {
+  const profileImage = await fetchProfileImage();
+  if (profileImage) {
+    return (
+      <Image
+        src={profileImage}
+        alt="Profile Image"
+        width={24}
+        height={24}
+        className="rounded-full"
+      />
+    );
+  }
   return <LuUser2 className="w-6 h-6 bg-primary rounded-full text-white" />;
 }
--- src/utils/actions.ts
+++ src/utils/actions.ts
@@ -32,4 +32,20 @@ export const createProfileAction = async (prevState: any, formData: FormData) =>
         }
     }
     redirect('/profile');
-};  
+};
+
+
+export const fetchProfileImage = async () => {
+    const user = await currentUser();
+    if (!user) return null;
+
+    const profile = await db.profile.findUnique({
+        where: {
+            clerkId: user.id,
+        },
+        select: {
+            profileImage: true,
+        },
+    })
+    return profile?.profileImage;
+}
