80344c0b3d3545986589c669504a0f65e9241ac0
RaminMikayilov <raminmikayilov2003@gmail.com>
2024-12-29 22:20:31 +0400

    feat(favorites): add favorites page

--- src/app/favorites/page.tsx
+++ src/app/favorites/page.tsx
@@ -1,3 +1,19 @@
-export default function FavoritesPage() {
-  return <h1 className="text-3xl">FavoritesPage</h1>;
+import EmptyList from "@/components/custom/home/EmptyList";
+import PropertiesList from "@/components/custom/home/PropertiesList";
+import { fetchFavorites } from "@/utils/actions";
+
+async function FavoritesPage() {
+  const favorites = await fetchFavorites();
+
+  if (favorites.length === 0) {
+    return <EmptyList />;
+  }
+
+  return (
+    <section>
+      <h1 className="text-3xl mb-2">Favorites Page</h1>
+      <PropertiesList properties={favorites} />
+    </section>
+  );
 }
+export default FavoritesPage;
