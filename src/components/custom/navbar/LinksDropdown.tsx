13d638a13604eb1f04019acbe310e4974d77675d
RaminMikayilov <raminmikayilov2003@gmail.com>
2025-01-26 20:49:47 +0400

    feat(admin): add admin page and restrict admin link visibility based on user role

--- src/components/custom/navbar/LinksDropdown.tsx
+++ src/components/custom/navbar/LinksDropdown.tsx
@@ -12,8 +12,12 @@ import { links } from "@/lib/constants/navLinks";
 import Link from "next/link";
 import SignOutLink from "./SignOutLink";
 import { SignedIn, SignedOut, SignInButton, SignUpButton } from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs/server";
 
-export default function DropdownMenuDemo() {
+export default function LinksDropdown() {
+  const { userId } = auth();
+  const isAdminUser = userId === process.env.ADMIN_USER_ID;
+
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
@@ -37,13 +41,16 @@ export default function DropdownMenuDemo() {
           </DropdownMenuItem>
         </SignedOut>
         <SignedIn>
-          {links.map((link) => (
-            <DropdownMenuItem key={link.label}>
-              <Link href={link.href} className="capitalize w-full">
-                {link.label}
-              </Link>
-            </DropdownMenuItem>
-          ))}
+          {links.map((link) => {
+            if (link.label === "admin" && !isAdminUser) return null;
+            return (
+              <DropdownMenuItem key={link.href}>
+                <Link href={link.href} className="capitalize w-full">
+                  {link.label}
+                </Link>
+              </DropdownMenuItem>
+            );
+          })}
           <DropdownMenuSeparator />
           <DropdownMenuItem>
             <SignOutLink />
